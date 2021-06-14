78c9be89e74e7f808507d362badb90a47e5371b2
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-06-14 13:11:23 +0200

    implementig home component

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'b6440de107mshae9780f6fde9a4dp19db7bjsn2689ac1a279f',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: 'e40e743af2c94b0c916a8aa618fb4473',
+      }
+
+    });
+    return next.handle(req)
+  }
+}
