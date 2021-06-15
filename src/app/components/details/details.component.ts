5f88c9f19cee31efc88586bb23d9b3108dd8f4da
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-06-15 13:03:39 +0200

    details component

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
-  gameRating = 0 
+export class DetailsComponent implements OnInit, OnDestroy {
+  gameRating = 0;
+  gameId: string ="";
+  game!: Game;
+  routeSub!: Subscription;
+  gameSub!: Subscription;
 
-  constructor() { }
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private httpServie: HttpService
+    
+  ) { }
 
   ngOnInit(): void {
+    this.routeSub = this.ActivatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id']
+      this.getGameDetails(this.gameId)
+    })
+  }
+  getGameDetails(id: string): void{
+    this.gameSub = this.httpServie
+    .getGameDetails(id)
+    .subscribe((gameResp: Game) => {
+      this.game = gameResp
+      setTimeout(() =>{
+        this.gameRating = this.game.metacritic
+        }, 1000)
+      })
   }
 
   getColor(value: number): string{
@@ -24,5 +50,14 @@ export class DetailsComponent implements OnInit {
       return '#ef4655'
     }
 
+  }
+  ngOnDestroy(){
+    if (this.gameSub){
+      this.gameSub.unsubscribe()
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe()
+    }
   }
 }
