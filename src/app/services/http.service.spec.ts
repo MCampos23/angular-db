fed325dcd8102dfc35553df5e398ac70018f0a22
Miguel Campos <63472933+MCampos23@users.noreply.github.com>
2021-08-14 23:26:03 +0200

    Update http.service.spec.ts

--- src/app/services/http.service.spec.ts
+++ src/app/services/http.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { HttpService } from './http.service';
 
 describe('HttpService', () => {
@@ -9,7 +8,6 @@ describe('HttpService', () => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(HttpService);
   });
-
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
